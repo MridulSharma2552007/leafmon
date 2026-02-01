a55a73aa8ac31829d2300afc63339ea6bd6281df
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-02-01 21:10:26 +0530

    Implement initial structure for CPU, MEM, and UI modules

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,27 @@
 #include <unistd.h>
 
 int main() {
-    initscr();
-    noecho();
-    curs_set(0);
-    nodelay(stdscr, TRUE);
+  initscr();
+  noecho();
+  curs_set(0);
+  nodelay(stdscr, TRUE); // make getch unblockable
+  while (true) {
+    clear();
+    mvprintw(1, 2, "leafmon");
+    mvprintw(3, 2, "CPU --");
+    mvprintw(5, 2, "MEM --");
+    mvprintw(7, 2, "Press q to quit");
 
-    while (true) {
-
-        clear();
-
-        mvprintw(1, 2, "leafmon");
-        mvprintw(3, 2, "Running...");
-        mvprintw(5, 2, "Press q to quit");
-
-        refresh();
-
-        int ch = getch();
-        if (ch == 'q')
-            break;
-
-        usleep(200000);
+    refresh();
+    int ch = getch();
+    if (ch == 'q') {
+      break;
     }
 
-    endwin();
-    return 0;
+    // sleep for 200 ms for less ram consumption
+    int sleepTime = 200000;
+    usleep(sleepTime);
+  }
+  endwin();
+  return 0;
 }
